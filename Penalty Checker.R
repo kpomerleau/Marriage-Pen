1db9d28a134e38c5eb382a689192270a37046ee6
Kyle Pomerleau <kylepomerleau@gmail.com>
2015-04-16 16:37:46 -0400

    Added Examples
    Added example couples to the code for the paper. Also added a marginal
    tax rate example.

--- Penalty Checker.R
+++ Penalty Checker.R
@@ -1,5 +1,5 @@
-#setwd("C:/Users/kep/Documents/GitHub/Marriage-Pen")
-setwd("C:/Users/Kyle/Documents/GitHub/Marriage-Pen")
+setwd("C:/Users/kep/Documents/GitHub/Marriage-Pen")
+#setwd("C:/Users/Kyle/Documents/GitHub/Marriage-Pen")
 
 rm(list=ls()) 
 
@@ -13,7 +13,7 @@ fedtax<-read.csv("fedtax.csv", header = TRUE, fill = TRUE, sep = ",")
 
 #data<-read.csv("data.csv", header = TRUE, fill = TRUE, sep = ",")
 
-data<-read.csv("newdata.csv", header = TRUE, fill = TRUE, sep = ",")
+data<-read.csv("newdata400.csv", header = TRUE, fill = TRUE, sep = ",")
 
 #Load Functions. One file is the basic tax functions, the burden functions are the 
 #calculations for the actual penalty/bonus
@@ -287,6 +287,234 @@ while(x <= length(data$multiplier)){
 
 #Manipulate Tables
 
+toutput_nochildren<-t(output_nochildren)
+toutput_onechild<-t(output_onechild)
+toutput_twochildren<-t(output_twochildren)
+
+#percent
+
+poutput_nochildren<-
+poutput_onechild<-t(output_onechild)
+poutput_twochildren<-t(output_twochildren)
+
+
+#non Transposed Tables
+
 write.table(output_nochildren,sep=",",file="nochildren_marriagepenalty.txt")
 write.table(output_onechild,sep=",",file="onechild_marriagepenalty.txt")
 write.table(output_twochildren,sep=",",file="twochildren_marriagepenalty.txt")
+
+#Transposed Tables
+
+write.table(toutput_nochildren,sep=",",file="tnochildren_marriagepenalty.txt")
+write.table(toutput_onechild,sep=",",file="tonechild_marriagepenalty.txt")
+write.table(toutput_twochildren,sep=",",file="ttwochildren_marriagepenalty.txt")
+
+#Examples
+
+  #Unequal Bonus
+  
+      children<-0
+      married<-0
+      hoh<-0
+      stateincometax<-0
+
+    #Person 1    
+  
+      income<-50000
+      taxableincome<-FedTaxableIncome(income,children,married,hoh,stateincometax)
+      incometax<-FedIncomeTax(taxableincome,married,hoh)
+      EITC<-FedEITC(income,children,married)
+      CTC<-FedCTC(income,children,married)
+      payrolltax<-FedEmployeePayroll(income,married)
+      totaltax<-(incometax-EITC-CTC)+payrolltax
+  
+      Unequalbonus<-matrix(
+        c(income,taxableincome,incometax,EITC,CTC,payrolltax,totaltax), 
+        nrow = 7, ncol=1, byrow=TRUE, 
+        dimnames = list(c("Income","Taxable Income","Income Tax","EITC","CTC","Payroll Tax","Total Tax"),c("Person1"))
+        )
+      
+    #Person 2
+  
+      income<-25000
+      taxableincome<-FedTaxableIncome(income,children,married,hoh,stateincometax)
+      incometax<-FedIncomeTax(taxableincome,married,hoh)
+      EITC<-FedEITC(income,children,married)
+      CTC<-FedCTC(income,children,married)
+      payrolltax<-FedEmployeePayroll(income,married)
+      totaltax<-(incometax-EITC-CTC)+payrolltax
+  
+      person2<-c(income,taxableincome,incometax,EITC,CTC,payrolltax,totaltax)
+      Unequalbonus<-cbind(Unequalbonus,person2)
+    
+      unmarriedcouple<-rowSums(Unequalbonus)
+      Unequalbonus<-cbind(Unequalbonus,unmarriedcouple)
+
+
+    #Married
+  
+      income<-75000
+      married<-1
+      taxableincome<-FedTaxableIncome(income,children,married,hoh,stateincometax)
+      incometax<-FedIncomeTax(taxableincome,married,hoh)
+      EITC<-FedEITC(income,children,married)
+      CTC<-FedCTC(income,children,married)
+      payrolltax<-Unequalbonus[6,1]+Unequalbonus[6,2]
+      totaltax<-(incometax-EITC-CTC)+payrolltax
+  
+      couple<-c(income,taxableincome,incometax,EITC,CTC,payrolltax,totaltax)
+      Unequalbonus<-cbind(Unequalbonus,couple)
+
+  #Low Income Penalty
+
+    married<-0
+    stateincometax<-0
+
+    #Person 1
+
+      children<-1
+      hoh<-1
+
+      income<-15000
+      taxableincome<-FedTaxableIncome(income,children,married,hoh,stateincometax)
+      incometax<-FedIncomeTax(taxableincome,married,hoh)
+      EITC<-FedEITC(income,children,married)
+      CTC<-FedCTC(income,children,married)
+      payrolltax<-FedEmployeePayroll(income,married)
+      totaltax<-(incometax-EITC-CTC)+payrolltax
+
+      Lowincomepenalty<-matrix(
+        c(income,taxableincome,incometax,EITC,CTC,payrolltax,totaltax), 
+        nrow = 7, ncol=1, byrow=TRUE, 
+        dimnames = list(c("Income","Taxable Income","Income Tax","EITC","CTC","Payroll Tax","Total Tax"),c("Person1"))
+      )
+
+    #Person 2
+
+      children<-0
+      hoh<-0
+      
+      income<-15000
+      taxableincome<-FedTaxableIncome(income,children,married,hoh,stateincometax)
+      incometax<-FedIncomeTax(taxableincome,married,hoh)
+      EITC<-FedEITC(income,children,married)
+      CTC<-FedCTC(income,children,married)
+      payrolltax<-FedEmployeePayroll(income,married)
+      totaltax<-(incometax-EITC-CTC)+payrolltax
+      
+      person2<-c(income,taxableincome,incometax,EITC,CTC,payrolltax,totaltax)
+      Lowincomepenalty<-cbind(Lowincomepenalty,person2)
+
+      unmarriedcouple<-rowSums(Lowincomepenalty)
+      Lowincomepenalty<-cbind(Lowincomepenalty,unmarriedcouple)
+
+
+    #Married
+    
+      income<-30000
+      married<-1
+      children<-1
+      taxableincome<-FedTaxableIncome(income,children,married,hoh,stateincometax)
+      incometax<-FedIncomeTax(taxableincome,married,hoh)
+      EITC<-FedEITC(income,children,married)
+      CTC<-FedCTC(income,children,married)
+      payrolltax<-Lowincomepenalty[6,1]+Lowincomepenalty[6,2]
+      totaltax<-(incometax-EITC-CTC)+payrolltax
+      
+      couple<-c(income,taxableincome,incometax,EITC,CTC,payrolltax,totaltax)
+      Lowincomepenalty<-cbind(Lowincomepenalty,couple)
+
+  #High Income Penalty
+
+    married<-0
+    stateincometax<-0
+
+    #Person 1
+    
+      children<-0
+      hoh<-0
+      
+      income<-150000
+      taxableincome<-FedTaxableIncome(income,children,married,hoh,stateincometax)
+      incometax<-FedIncomeTax(taxableincome,married,hoh)
+      EITC<-FedEITC(income,children,married)
+      CTC<-FedCTC(income,children,married)
+      payrolltax<-FedEmployeePayroll(income,married)
+      medicaretax<-MedSurtax(income,married)
+      amt<-AMT(income,married)
+      totaltax<-max(amt,(incometax-EITC-CTC))+payrolltax+medicaretax
+      
+      Highincomepenalty<-matrix(
+        c(income,taxableincome,incometax,EITC,CTC,payrolltax,medicaretax,amt,totaltax), 
+        nrow = 9, ncol=1, byrow=TRUE, 
+        dimnames = list(c("Income","Taxable Income","Income Tax","EITC","CTC","Payroll Tax","Medicare Surtax","AMT","Total Tax"),c("Person1"))
+      )
+
+    #Person 2
+    
+      children<-0
+      hoh<-0
+      
+      income<-150000
+      taxableincome<-FedTaxableIncome(income,children,married,hoh,stateincometax)
+      incometax<-FedIncomeTax(taxableincome,married,hoh)
+      EITC<-FedEITC(income,children,married)
+      CTC<-FedCTC(income,children,married)
+      payrolltax<-FedEmployeePayroll(income,married)
+      medicaretax<-MedSurtax(income,married)
+      amt<-AMT(income,married)
+      totaltax<-max(amt,(incometax-EITC-CTC))+payrolltax+medicaretax
+      
+      person2<-c(income,taxableincome,incometax,EITC,CTC,payrolltax,medicaretax,amt,totaltax)
+      Highincomepenalty<-cbind(Highincomepenalty,person2)
+      
+      unmarriedcouple<-rowSums(Highincomepenalty)
+      Highincomepenalty<-cbind(Highincomepenalty,unmarriedcouple)
+
+    #Married Couple
+
+      children<-0
+      married<-1
+      
+      income<-300000
+      taxableincome<-FedTaxableIncome(income,children,married,hoh,stateincometax)
+      incometax<-FedIncomeTax(taxableincome,married,hoh)
+      EITC<-FedEITC(income,children,married)
+      CTC<-FedCTC(income,children,married)
+      payrolltax<-FedEmployeePayroll(income,married)
+      medicaretax<-MedSurtax(income,married)
+      amt<-AMT(income,married)
+      totaltax<-max(amt,(incometax-EITC-CTC))+payrolltax+medicaretax
+
+      couple<-c(income,taxableincome,incometax,EITC,CTC,payrolltax,medicaretax,amt,totaltax)
+      Highincomepenalty<-cbind(Highincomepenalty,couple)
+
+  #Marginal rate example
+
+
+      children<-0
+      hoh<-0
+      married<-1
+      income<-50000
+      taxableincome<-FedTaxableIncome(income,children,married,hoh,stateincometax)
+      incometax<-FedIncomeTax(taxableincome,married,hoh)
+      EITC<-FedEITC(income,children,married)
+      CTC<-FedCTC(income,children,married)
+      payrolltax<-FedEmployeePayroll(income,married)
+      medicaretax<-MedSurtax(income,married)
+      amt<-AMT(income,married)
+      totaltax1<-max(amt,(incometax-EITC-CTC))+payrolltax+medicaretax
+      
+      income<-50001
+      taxableincome<-FedTaxableIncome(income,children,married,hoh,stateincometax)
+      incometax<-FedIncomeTax(taxableincome,married,hoh)
+      EITC<-FedEITC(income,children,married)
+      CTC<-FedCTC(income,children,married)
+      payrolltax<-FedEmployeePayroll(income,married)
+      medicaretax<-MedSurtax(income,married)
+      amt<-AMT(income,married)
+      totaltax2<-max(amt,(incometax-EITC-CTC))+payrolltax+medicaretax
+    
+      marginalrate1<-totaltax2-totaltax1
+
